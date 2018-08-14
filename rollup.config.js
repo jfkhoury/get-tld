a459011e147244f05dbb9be7b658dd0414c34102
Joe Khoury <khoury@adobe.com>
2018-08-14 15:17:15 -0400

    Initial commit.

---
+++ rollup.config.js
@@ -0,0 +1,20 @@
+import babel from 'rollup-plugin-babel';
+import minify from 'rollup-plugin-minify-es';
+
+export default {
+  input: 'src/getTld.js',
+  output: {
+    file: 'dist/getTld.js',
+    format: 'umd',
+    name: "getTld"
+  },
+  plugins: [
+    babel({
+      exclude: 'node_modules/**',
+      babelrc: false,
+      presets: [['env', { modules: false }]]
+    }
+  ),
+    minify()
+  ]
+};
