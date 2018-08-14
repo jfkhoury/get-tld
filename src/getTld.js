ff71e009f3c54bdbf5446fd65c2e3ef60449309d
Joe Khoury <khoury@adobe.com>
2018-08-14 15:23:48 -0400

    Quick refactor.

--- src/getTld.js
+++ src/getTld.js
@@ -1,26 +1,19 @@
 export default function getTld() {
     const parts = location.hostname.toLowerCase().split(".");
-    const cookieName = "_FIND_DOMAIN_" + (new Date()).getTime(); // Add timestamp to make it unique.
+    const cookieName = `_FIND_DOMAIN_${(new Date()).getTime()}`; // Add timestamp to make it unique.
 
     if (parts.length === 1) { // If no `.` found, then maybe we're on localhost.
         return "";
     }
     
-    function isCookieNotFound() {
-        return global.document.cookie.indexOf(cookieName+'='+cookieName) === -1;
-    }
-
-    function getArrayItemsFromEnd(arr, nbrOfItems) {
-        return arr.slice(-nbrOfItems);
-    }
+    const isCookieNotFound = () => global.document.cookie.indexOf(cookieName+'='+cookieName) === -1;
+    const getArrayItemsFromEnd = (arr, nbrOfItems) => arr.slice(-nbrOfItems);
+    const expireCookie = domain =>
+        global.document.cookie = `${cookieName}=;expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=${domain};`;
 
     function findTld() {
         let i = 0;
         let domain = "";
-
-        function expireCookie(domain) {
-            global.document.cookie = cookieName + "=;expires=Thu, 01 Jan 1970 00:00:01 GMT;domain=" + domain + ";";
-        }
 
         while(i < (parts.length - 1) && isCookieNotFound()) {
             domain = getArrayItemsFromEnd(parts, 1 + (++i)).join(".");
